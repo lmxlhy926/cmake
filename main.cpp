f217ecb5bb32c3a6fded5fbecc20a1889b88fc3f
lhy <784723153@qq.com>
2020-11-18 18:03:50 +0500

    2020/11/18

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
+
+
 #include <iostream>
+#include <config.h>
+
+using namespace std;
+
+#ifdef MATHLIB
+#include "add.h"
+#endif
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    cout << "major: " << aaa << endl;
+    cout << "minor: " << bbb << endl;
+
+#ifdef MATHLIB
+    cout << "add: " << add(1, 2) << endl;
+#endif
+
+
     return 0;
 }
